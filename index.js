daebc83db3f511fe618169b96fadfe8cd7b5dbb9
Rasel9360 <raselmahamud9360@gmail.com>
2024-05-13 23:39:15 +0600

    firebase domain added

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express();
 // middleware setup
 app.use(cors(
   {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://assignment-eleven-74640.web.app',
+      'https://assignment-eleven-74640.firebaseapp.com'
+    ],
     credentials: true,
   }
 ));
@@ -36,6 +41,30 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middleware
+
+const logger = (req, res, next) => {
+  console.log("user info", req.method, req.url)
+  next()
+}
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log("token in the middle", token);
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" })
+  }
+  // verifying the token using jwt library
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: "unauthorized access" })
+    }
+    req.user = decoded;
+    next()
+  })
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -46,18 +75,18 @@ async function run() {
 
 
     // jwt related api
-    app.post('/jwt', async(req, res) => {
+    app.post('/jwt', logger, async (req, res) => {
       const user = req.body;
       console.log("token", user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '7d'});
-      res.cookie('token', token, cookieOptions).send({success: true});
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' });
+      res.cookie('token', token, cookieOptions).send({ success: true });
     })
 
     // clear token
-    app.post('/logout', async (req, res)=>{
+    app.post('/logout', logger, async (req, res) => {
       const user = req.body;
       console.log("clear cookie", user);
-      res.clearCookie("token", {...cookieOptions, maxAge: 0}).send({success: true})
+      res.clearCookie("token", { ...cookieOptions, maxAge: 0 }).send({ success: true })
     })
 
 
@@ -67,10 +96,10 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/blogs/:id', async(req, res) => {
+    app.get('/blogs/:id', async (req, res) => {
       const id = req.params.id;
       // console.log(id);
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await blogsCollection.findOne(query);
       res.send(result);
     })
@@ -79,17 +108,17 @@ async function run() {
       const filter = req.query.filter;
       const search = req.query.search;
       let query = {
-        title: {$regex: search, $options: 'i'}
+        title: { $regex: search, $options: 'i' }
       };
-      if (filter) query = {...query, category: filter };
+      if (filter) query = { ...query, category: filter };
       const result = await blogsCollection.find(query).toArray();
       res.send(result);
     })
 
-   
+
 
     // post blogs
-    app.post('/blogs', async (req, res) => {
+    app.post('/blogs', logger, async (req, res) => {
       const query = req.body;
       // console.log(query);
       const result = await blogsCollection.insertOne(query);
@@ -97,14 +126,14 @@ async function run() {
     })
 
     // comments related API
-    app.post('/comment', async(req, res) => {
+    app.post('/comment', async (req, res) => {
       const comment = req.body;
       // console.log(comment);
       const result = await commentCollection.insertOne(comment);
       res.send(result);
     })
 
-    app.get("/comment/:blogId", async (req, res)=>{
+    app.get("/comment/:blogId", async (req, res) => {
       // console.log(req.params.email)
       const query = { blogId: req.params.blogId };
       // console.log(query);
@@ -113,9 +142,9 @@ async function run() {
     })
 
     // Update blog
-    app.put('/updateBlog/:id', async(req, res) => {
+    app.put('/updateBlog/:id', logger, async (req, res) => {
       const blogsDta = req.body;
-      const query = {_id: new ObjectId(req.params.id)};
+      const query = { _id: new ObjectId(req.params.id) };
       const options = { upsert: true };
       const updateDoc = {
         $set: {
